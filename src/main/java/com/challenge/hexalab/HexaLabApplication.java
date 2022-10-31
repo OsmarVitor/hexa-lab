c89f62e9130cac953ccd8b28185e6a79e8c891ce
OsmarVitor <perez_vitor@hotmail.com>
2022-10-31 16:06:47 -0300

    feat: first commit

---
+++ src/main/java/com/challenge/hexalab/HexaLabApplication.java
@@ -0,0 +1,12 @@
+package com.challenge.hexalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexaLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexaLabApplication.class, args);
+	}
+}
---
+++ src/test/java/com/challenge/hexalab/HexaLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.hexalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexaLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
